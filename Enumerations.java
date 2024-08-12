0f4051c76b85d8714e20b0683627cb58ee26271f
psvsai <pasupuletisvsm245@gmail.com>
2024-08-12 21:02:36 +0530

    PSVSAI

--- Enumerations.java
+++ Enumerations.java
@@ -1,3 +1,4 @@
+import java.util.Enumeration;
 import java.util.Stack;
 
 public class Enumerations{
@@ -7,11 +8,11 @@ public class Enumerations{
   // Iterate it by using Enumeration
   // print the values by using while Loop
   
-    Stack<person> stack = new Stack<>();
+    Stack<Person> stack = new Stack<>();
     stack.push(new Person("Karthik", 21));
     stack.push(new Person("Sai", 20));
 
-   Enumerations<person> enumeration = stack.elements();
+   Enumeration<Person> enumeration = stack.elements();
     while (enumeration.hasMoreElements()) {
         Person person = enumeration.nextElement();
         System.out.println("Person Name: " + person.getName() + ", Age: " + person.getAge());
